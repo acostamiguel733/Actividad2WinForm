eb7e761a1890821baa22da3a0c5840113b69cc83
acostamiguel733 <miguel.acosta@alumnos.frgp.utn.edu.ar>
2022-04-26 19:10:50 -0300

    Primer commit

---
+++ WinformsActividad2/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformsActividad2
+{
+    internal class Articulo
+    {
+        public int CodArt { get; set; }
+        public string Nombre { get; set; }  
+        public string Descripcion { get; set; } 
+        public string Marca { get; set; }   
+        public string Categoria { get; set; }   
+        public float Precio { get; set; }   
+        public bool Estado { get; set; }    
+
+    }
+}
