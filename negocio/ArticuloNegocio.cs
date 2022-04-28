81700d529f7f3adfbe035efaa264f573814955ef
acostamiguel733 <miguel.acosta@alumnos.frgp.utn.edu.ar>
2022-04-28 20:25:22 -0300

    Clase ArticuloNegocio con conexion a BD lista

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using System.Data.SqlClient;
+
+
+namespace negocio
+{
+    public  class ArticuloNegocio
+    {
+        public List<Articulo> Listar()
+        {
+            List <Articulo> list = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+             
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; datebase=CATALOGO_DB; integrated security=true" ;
+                   return list;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+    }
+}
